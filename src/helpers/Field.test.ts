0d106cf686b2ea004988e2a0487f16f72c3a4ad4
Joe Contreras <joe.contreras888@gmail.com>
2022-03-21 21:09:05 -0600

    bomb location randomized

---
+++ src/helpers/Field.test.ts
@@ -0,0 +1,74 @@
+import { CellState, emptyFieldGenerator, fieldGenerator } from "./Field";
+const { empty, bomb, hidden } = CellState;
+
+describe("Field Generator", () => {
+  describe("emptyFieldGenerator tests", () => {
+    it("2x2", () => {
+      expect(emptyFieldGenerator(2)).toStrictEqual([
+        [empty, empty],
+        [empty, empty],
+      ]);
+    });
+    it("3x3", () => {
+      expect(emptyFieldGenerator(3)).toStrictEqual([
+        [empty, empty, empty],
+        [empty, empty, empty],
+        [empty, empty, empty],
+      ]);
+    });
+    it("3x3 with hidden state", () => {
+      expect(emptyFieldGenerator(3, hidden)).toStrictEqual([
+        [hidden, hidden, hidden],
+        [hidden, hidden, hidden],
+        [hidden, hidden, hidden],
+      ]);
+    });
+  });
+
+  describe("Simple Cases", () => {
+    it("wrong density", () => {
+      const errorText = "Probability must be between 0 and 1";
+      expect(() => fieldGenerator(1, -1)).toThrow(errorText);
+      expect(() => fieldGenerator(1, 2)).toThrow(errorText);
+    });
+    it("smallest possible field without mine", () => {
+      expect(fieldGenerator(1, 0)).toStrictEqual([[empty]]);
+    });
+    it("large field without mine", () => {
+      expect(fieldGenerator(10, 0)).toStrictEqual([
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+      ]);
+    });
+    it("smallest possible field with mine", () => {
+      expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]]);
+    });
+    it("2x2 field with mines", () => {
+      expect(fieldGenerator(2, 1)).toStrictEqual([
+        [bomb, bomb],
+        [bomb, bomb],
+      ]);
+    });
+    it("2x2 field with 50% probability", () => {
+      const field = fieldGenerator(2, 0.5);
+      const flatField = field.flat();
+
+      console.table(field);
+      console.table(flatField);
+
+      const cellsWithBombs = flatField.filter((cell) => cell === bomb);
+      const emptyCells = flatField.filter((cell) => cell === empty);
+
+      expect(cellsWithBombs).toHaveLength(2);
+      expect(emptyCells).toHaveLength(2);
+    });
+  });
+});
