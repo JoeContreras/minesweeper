cbe0141b7c842f2301522642e234cf64ead63608
Joe Contreras <joe.contreras888@gmail.com>
2022-03-28 20:54:19 -0600

    Top stories works

---
+++ src/components/Top/Top.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { FC } from "react";
+import { GameName, GameNameProps } from "./GameName";
+import { Legend, LegendProps } from "./Legend";
+
+export type TopComponentType = GameNameProps & LegendProps;
+
+export const Top: FC<TopComponentType> = ({ children, ...legendProps }) => (
+  <Header>
+    <GameName>{children}</GameName>
+    <Legend {...legendProps} />
+  </Header>
+);
+
+const Header = styled.header`
+  text-align: center;
+  position: relative;
+  display: inline-block;
+`;
